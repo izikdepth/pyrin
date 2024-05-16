761613a40dfe87d360b0bf41fde53a09a296f7d2
izik <bwanfomisaac@gmail.com>
2024-05-16 01:45:31 +0100

    fix: fix Cogs loading issue

--- pyrin.py
+++ pyrin.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
 bot = commands.Bot(command_prefix="/")
 
-#intents
+# intents
 intents = discord.Intents.default()
 intents.members = True
 
@@ -15,12 +16,23 @@ intents.members = True
 async def on_ready():
     print("Bot is ready!")
 
-Cogs_list = [
-    "stats", 
-]
+async def load_extensions():
+    Cogs_list = ["stats"]  
+    for cog_name in Cogs_list:
+        try:
+            bot.load_extension(f'Cogs.{cog_name}')
+            print(f"Loaded extension: Cogs.{cog_name}")
+        except ImportError as e:
+            print(f"Failed to load extension: Cogs.{cog_name}. Error: Module not found {e}.")
+        except AttributeError as e:
+            print(f"Failed to load extension: Cogs.{cog_name}. Error: Attribute error {e}.")
 
-for Cog in Cogs_list:
-    bot.load_extension(f'Cogs.{Cog}')
 
-Token = os.getenv("PYRIN_BOT")
-bot.run(Token)
+
+async def main():
+    await load_extensions()
+    Token = os.getenv("PYRIN_BOT")
+    await bot.start(Token)
+
+if __name__ == "__main__":
+    asyncio.run(main())
